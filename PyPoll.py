01a79df6e4674897017b5e5970feebf0c7459e31
Ranger9103 <kateheise@yahoo.com>
2022-04-05 10:47:41 -0500

    candidate names

--- PyPoll.py
+++ PyPoll.py
@@ -1,27 +1,17 @@
 import csv
 import os
-#Assign a variable to load a file from a path.
-file_to_load=os.path.join("Resources","election_results.csv")
-#Assign a variable to save the file to a path.
-file_to_save=os.path.join("analysis","election_analysis.txt")
-
-#Open the election results and read the file.
+file_to_load=os.path.join("Resources", "election_results.csv")
+file_to_save=os.path.join("analysis", "election_analysis.txt")
+total_votes=0
+candidate_options=[]
 with open(file_to_load) as election_data:
-
-    #To do: read and analyze the data here.
-    #Read the file object with the reader function.
     file_reader=csv.reader(election_data)
-    #Print the row header.
     headers=next(file_reader)
-    print(headers)
+    for row in file_reader:
+        total_votes+=1
+        candidate_name=row[2]
+    
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
 
-#1. The total number of votes cast.
-#2. A complete list of candidates who received votes.
-#3. The percentage of votes each candidate won.
-#4. The total number of votes each candidate won.
-#5. The winnter of the election based on popular vote.
-
-
-
-
-
+print(candidate_options)
