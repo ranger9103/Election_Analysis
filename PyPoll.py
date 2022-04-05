b3e55d75f655cc03db94cb3d7cb15d53ce036cb0
Ranger9103 <kateheise@yahoo.com>
2022-04-05 11:01:42 -0500

    vote counter

--- PyPoll.py
+++ PyPoll.py
@@ -4,6 +4,7 @@ file_to_load=os.path.join("Resources", "election_results.csv")
 file_to_save=os.path.join("analysis", "election_analysis.txt")
 total_votes=0
 candidate_options=[]
+candidate_votes={}
 with open(file_to_load) as election_data:
     file_reader=csv.reader(election_data)
     headers=next(file_reader)
@@ -13,5 +14,9 @@ with open(file_to_load) as election_data:
     
         if candidate_name not in candidate_options:
             candidate_options.append(candidate_name)
+            candidate_votes[candidate_name]=0
+        candidate_votes[candidate_name]+=1
 
-print(candidate_options)
+
+
+print(candidate_votes)
